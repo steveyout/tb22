cdd5c2381a6cef3146516a01835180c4fd49c02c
Michaeldgeek <petermichael908@gmail.com>
2017-09-03 14:00:48 +0000

    rubbish

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'localhost:3306',
+    host: 'localhost',
     user: 'root',
     password: 'android908',
     database: 'telegram'
